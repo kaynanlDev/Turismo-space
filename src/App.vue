fe89364e086e0c12d3b218520eb66b0fdc19be3f
kaynanDev <kaynan2817@gmail.com>
2025-08-03 21:20:11 -0300

    Aplicação de responsividade e criação de link da Home

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ let openMenu = ref(false)
 </script>
 
 <template>
-  <header class="w-full  relative pt-10 sm:pt-0 md:pt-10 pl-10 flex items-center justify-between">
+  <header class="w-full  pt-10 sm:pt-0 md:pt-10 pl-10 flex items-center justify-between">
     <div>
       <RouterLink to="/" class="w-3/4"><img :src="logo" alt="logotipo"></RouterLink>
     </div>
@@ -24,9 +24,9 @@ let openMenu = ref(false)
       <RouterLink to="/tecnologia" class="font-barlow-c uppercase"><span class="font-bold mr-3">03</span>Tecnologia</RouterLink>
     </nav>
     <button v-if="openMenu === false" @click="openMenu = !openMenu" class="sm:hidden pr-8 cursor-pointer"><img :src="menu" alt="icon menu"></button>
-    <div v-if="openMenu === true"  class="bg-[#ffffff0a] absolute pt-8 pr-6 pl-8 top-0 right-px w-3/5 sm:hidden h-screen backdrop-blur-lg animate-fade">
+    <div v-if="openMenu === true"  class="bg-[#0b0d17fd] min-h-screen absolute pt-8 pr-6 pl-8 top-0 right-px w-3/5 sm:hidden   animate-fade">
       <button @click="openMenu = !openMenu" class="w-full mb-20 flex justify-end cursor-pointer"><img :src="close" alt="icon close"></button>
-      <nav class="menu-mobile   flex flex-col gap-8">
+      <nav class="menu-mobile h-full   flex flex-col gap-8">
         <RouterLink to="/" class="font-barlow-c uppercase"><span class="font-bold mr-3">00</span>Home</RouterLink>
         <RouterLink to="/destino" class="font-barlow-c uppercase"><span class="font-bold mr-3">01</span>Destino</RouterLink>
         <RouterLink to="/equipe" class="font-barlow-c uppercase"><span class="font-bold mr-3">02</span>Equipe</RouterLink>
@@ -38,19 +38,32 @@ let openMenu = ref(false)
 </template>
 <style>
 
-body{
+html,body{
+  margin: 0;
+  padding: 0;
   width: 100%;
-  height: 100vh;
-  background: url('./assets/back-1.jpg')no-repeat center / cover;
+  height: 100%;
+  overflow-x: hidden;
+}
+
+body{
+  background: url('./assets/back-1.jpg')no-repeat;
+  background-position: center;
+  background-size: cover;
+  backdrop-filter: brightness(60%);
   color: #fff;
-  overflow: hidden;
+}
+
+#app{
+  min-height: 100vh;
 }
 
 @media screen and (max-width: 48rem) {
   body{
     background: url('./assets/back-mobile.jpg')no-repeat center / cover;
     width: 100%;
-    height: 100vh;
+    height: 100%;
+     backdrop-filter: brightness(100%);
   }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <script setup></script>
 <template>
-    <div class="w-3/4 h-screen my-0 mx-auto flex flex-col md:flex-row py-32">
-        <div class="w-3/6 md:w-full h-[80%] flex flex-col justify-center">
-            <h3 class="w-full font-barlow-c tracking-[0.25rem] text-blue uppercase text-2xl border border-amber-200 text-center md:text-start">então você quer viajar para</h3>
-            <h1 class="text-white font-bellefair uppercase text-[9rem] text-center md:text-start">espaço</h1>
-            <p class="font-barlow text-blue text-[1.125rem] leading-[180%] text-center md:text-start">
+    <div class="w-3/4 h-full my-0 mx-auto flex flex-col items-center lg:flex-row py-32">
+        <div class="w-full h-[80%] flex flex-col justify-center">
+            <h3 class="w-full font-barlow-c tracking-[0.25rem] text-blue uppercase text-[1rem] sm:text-2xl text-center lg:text-start">então você quer viajar para</h3>
+            <h1 class="w-full text-white font-bellefair uppercase text-[5rem] md:text-[9rem] text-center lg:text-start">espaço</h1>
+            <p class="lg:w-[75%] font-barlow text-blue text-[0.938rem] sm:text-[1.125rem] leading-[180%] text-center lg:text-start  lg:mt-0">
                 Sejamos realistas: se você quer ir para o espaço, é melhor ir de verdade para o espaço sideral
                 e não ficar pairando na borda. Bem, sente-se e relaxe, porque nós lhe daremos uma experiência
                 verdadeiramente de outro mundo!
             </p>
         </div>
-        <div class="w-3/6 h-[80%] flex justify-end items-center"><button class="w-[17rem] h-[17rem] rounded-full bg-white text-black font-bellefair uppercase text-[2rem] cursor-pointer">Explore</button></div>
-        
+        <div class="w-full sm:w-3/6 h-[80%] flex justify-center  mt-[4rem] md:mt-[6rem] lg:ml-6  lg:justify-end items-center"><router-link to="/destino" class="w-[7.125rem] h-[7.125rem] flex items-center justify-center sm:w-[16rem] sm:h-[16rem] md:w-[17rem] md:h-[17rem] rounded-full bg-white text-black font-bellefair uppercase text-[1.125rem] sm:text-[2rem] cursor-pointer transition duration-[0.3s] ease-in-out hover:ring-40 hover:ring-[#ffffff10]">Explore</router-link></div>   
     </div>
-
 </template>
 <style></style>
