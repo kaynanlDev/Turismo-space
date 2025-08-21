2dc7469ed6ffa67fd844cc3c1f912f1e9d25f1a7
kaynanDev <kaynan2817@gmail.com>
2025-08-20 23:14:15 -0300

    Adição de estrutura e estilização da view Tecnologia

--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,17 @@ let openMenu = ref(false)
 function backHome() {
   document.body.className = ''
 }
+
 function backDestin() {
   document.body.className = 'backDestin'
 }
 
 function backCrew() {
   document.body.className = 'backCrew'
+}
+
+function backTec() {
+  document.body.className = 'backTec'
 }
 
 </script>
@@ -30,7 +35,7 @@ function backCrew() {
       <RouterLink to="/" @click="backHome" class="font-barlow-c uppercase"><span class="font-bold mr-3">00</span>Home</RouterLink>
       <RouterLink to="/destino" @click="backDestin" class="font-barlow-c uppercase"><span class="font-bold mr-3">01</span>Destino</RouterLink>
       <RouterLink to="/equipe" @click="backCrew" class="font-barlow-c uppercase"><span class="font-bold mr-3">02</span>Equipe</RouterLink>
-      <RouterLink to="/tecnologia" class="font-barlow-c uppercase"><span class="font-bold mr-3">03</span>Tecnologia</RouterLink>
+      <RouterLink to="/tecnologia" @click="backTec" class="font-barlow-c uppercase"><span class="font-bold mr-3">03</span>Tecnologia</RouterLink>
     </nav>
     <button v-if="openMenu === false" @click="openMenu = !openMenu" class="sm:hidden pr-8 cursor-pointer"><img :src="menu" alt="icon menu"></button>
     <div  v-if="openMenu === true"  class="menu-m bg-[#0b0d17fd] min-h-screen absolute pt-8 pr-6 pl-8 top-0 right-px w-3/5 sm:hidden animate-fade">
@@ -39,7 +44,7 @@ function backCrew() {
         <RouterLink to="/" @click="openMenu = false; backHome()" class="link font-barlow-c uppercase"><span class="font-bold mr-3">00</span>Home</RouterLink>
         <RouterLink to="/destino" @click="openMenu = false; backDestin()" class="font-barlow-c uppercase"><span class="font-bold mr-3">01</span>Destino</RouterLink>
         <RouterLink to="/equipe" @click="openMenu = false; backCrew()" class="font-barlow-c uppercase"><span class="font-bold mr-3">02</span>Equipe</RouterLink>
-        <RouterLink to="/tecnologia" @click="openMenu = false" class="font-barlow-c uppercase"><span class="font-bold mr-3">03</span>Tecnologia</RouterLink>
+        <RouterLink to="/tecnologia" @click="openMenu = false; backTec()" class="font-barlow-c uppercase"><span class="font-bold mr-3">03</span>Tecnologia</RouterLink>
       </nav>
     </div>
   </header>
@@ -56,7 +61,7 @@ html,body{
 }
 
 body{
-  background: url('./assets/back-home.jpg')no-repeat;
+  background: url('./assets/bg-home.jpg')no-repeat;
   background-position: center;
   background-size: cover;
   backdrop-filter: brightness(50%);
@@ -83,7 +88,13 @@ body{
   backdrop-filter: brightness(50%);
   color: #fff;
 }
-
+.backTec{
+  background: url('./assets/bg-Tec.jpg')no-repeat;
+  background-position: center;
+  background-size: cover;
+  backdrop-filter: brightness(50%);
+  color: #fff;
+}
 
 
 </style>
--- src/views/TecnologiaView.vue
+++ src/views/TecnologiaView.vue
@@ -1,5 +1,31 @@
-<script setup></script>
+<script setup>
+import tecA from '@/assets/tec-a.jpg'
+import tecB from '@/assets/tec-b.jpg'
+import tecC from '@/assets/tec-c.jpg'
+import DestinoSub from '@/components/DestinoSub.vue';
+</script>
 <template>
-    <h1>tecnologia</h1>
+    <div class="w-full  h-screen my-0 mx-auto pl-[10.313rem] animate-fade">
+        <DestinoSub destinSub="lançamentto espacial 101" destinNum="03"/>
+        <div class="flex items-center w-ful">
+            <div class="w-3/5 flex ">
+                <ul class="h-full mr-16 flex flex-col gap-8">
+                    <li class="flex items-center justify-center w-[5rem] h-[5rem] border border-[#ffffff25] rounded-full cursor-pointer"><button>1</button></li>
+                    <li class="flex items-center justify-center w-[5rem] h-[5rem] border border-[#ffffff25] rounded-full cursor-pointer"><button>2</button></li>
+                    <li class="flex items-center justify-center w-[5rem] h-[5rem] border border-[#ffffff25] rounded-full cursor-pointer"><button>3</button></li>
+                </ul>
+                <div>
+                    <h2 class="font-bellefair uppercase text-[1.125rem] sm:text-2xl lg:text-[2rem] opacity-50 text-center lg:text-start">a terminologia…</h2>
+                    <h1 class="font-bellefair uppercase text-2xl sm:text-[2.5rem] lg:text-[3.5rem] mt-4 mb-6 text-center lg:text-start">spaceport</h1>
+                    <p class="max-w-3/4 mx-auto lg:mx-0 font-barlow text-[1.125rem] text-blue text-center lg:text-start">
+                        Um espaçoporto ou cosmódromo é um local para lançamento (ou recebimento) de naves espaciais, semelhante
+                        ao porto marítimo para navios ou ao aeroporto para aeronaves. Localizado no famoso Cabo Canaveral, nosso
+                        espaçoporto está idealmente situado para aproveitar a rotação da Terra para lançamentos.
+                    </p>
+                </div>
+            </div>
+            <div class="w-3/7"><img :src="tecA" alt=""></div>
+        </div>
+    </div>
 </template>
 <style></style>
