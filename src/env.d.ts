152a805ee8f752643268a8c947a5348b1a8b8d54
kaynanDev <kaynan2817@gmail.com>
2025-08-24 19:27:46 -0300

    correções de bugs no Typescript

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="vite/client" />
+
 declare module '*.vue' {
     import type { DefineComponent } from 'vue'
     const component: DefineComponent<{}, {}, any>
@@ -7,4 +9,26 @@ declare module '*.vue' {
 declare module '*.png' {
     const value: string
     export default value
+}
+declare module '*.jpg' {
+    const value: string
+    export default value
+}
+declare module '*.jpeg' {
+    const value: string
+    export default value
+}
+declare module '*.svg' {
+    const value: string
+    export default value
+}
+
+interface ImportMetaEnv {
+    readonly VITE_BASE_URL: string
+    readonly VITE_APP_TITLE?: string
+    readonly [key: string]: string | undefined
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
 }
